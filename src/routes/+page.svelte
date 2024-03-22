7c0913255a90d24e94bad9a9683dab1c4b31c42d
Tej Chajed <chajed@wisc.edu>
2024-03-22 08:18:15 -0500

    Add flowbite UI library

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
 <h1>Interview to calendar export</h1>
 
 <p>
-  <label for="url">Sheet URL</label>
-  <input type="text" id="url" name="url" placeholder="https://docs.google.com/..." />
+	<label for="url">Sheet URL</label>
+	<input type="text" id="url" name="url" placeholder="https://docs.google.com/..." />
 </p>
 
 <p>
-  <button>Download ICS</button>
+	<button>Download ICS</button>
 </p>
 
 <style>
-  :global(body) {
-    max-width: 800px;
-    padding: 5px;
-    margin: 1em auto;
-  }
-  input#url {
-    width: 100%;
-  }
+	:global(body) {
+		max-width: 800px;
+		padding: 5px;
+		margin: 1em auto;
+	}
+	input#url {
+		width: 100%;
+	}
 </style>
