ffe3b56da7634065a76c7ef24c7a874965f2bac4
Tej Chajed <chajed@wisc.edu>
2024-03-21 21:28:09 -0500

    Add very basic UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Interview to calendar export</h1>
+
+<p>
+  <label for="url">Sheet URL</label>
+  <input type="text" id="url" name="url" placeholder="https://docs.google.com/..." />
+</p>
+
+<p>
+  <button>Download ICS</button>
+</p>
+
+<style>
+  :global(body) {
+    max-width: 800px;
+    padding: 5px;
+    margin: 1em auto;
+  }
+  input#url {
+    width: 100%;
+  }
+</style>
