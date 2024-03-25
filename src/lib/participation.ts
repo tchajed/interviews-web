aaef28a5daea8fad075e72213cfdae6dc1c2b56b
Tej Chajed <chajed@wisc.edu>
2024-03-25 16:25:57 -0500

    Basically get participation working
    Normalization and filtering isn't perfect but it's quite good now.
    Should provide a way to export raw data and do post-processing elsewhere
    (including graphing, for example).

--- src/lib/participation.ts
+++ src/lib/participation.ts
@@ -26,10 +26,6 @@ async function getSchedules(urls: string[]): Promise<Schedule[]> {
 			continue;
 		}
 		scheds.push(sheetDataToSchedule(tsv));
-		// TODO: remove, only for debugging
-		if (scheds.length >= 5) {
-			break;
-		}
 	}
 	return scheds;
 }
@@ -39,6 +35,7 @@ type PartType = "breakfast" | "lunch" | "1:1" | "dinner";
 export type ParticipationEvent = {
 	name: string;
 	type: PartType;
+	candidate: string;
 };
 
 function classifyEvent(row: ScheduleRow): PartType {
@@ -54,13 +51,13 @@ function classifyEvent(row: ScheduleRow): PartType {
 	return "1:1";
 }
 
-const IgnoredNames = new RegExp(`(BREAK|TALK|TALK PREP)|(^$)`, "i");
+const IgnoredNames = new RegExp(`(BREAK|TALK|TALK PREP|LUNCH|DINNER)|(^$)`, "i");
 
-export async function getParticipationEvents(masterHtml: string): Promise<ParticipationEvent[]> {
+async function getParticipationEvents(masterHtml: string): Promise<ParticipationEvent[]> {
 	const urls = getScheduleSheets(masterHtml);
 	const schedules = await getSchedules(urls);
 	const events: ParticipationEvent[] = [];
-	const re = new RegExp("\\s*[,+;]\\s*");
+	const re = new RegExp("\\s*[,+;&]\\s*");
 	for (const sched of schedules) {
 		for (const event of sched.events) {
 			const type = classifyEvent(event);
@@ -71,9 +68,105 @@ export async function getParticipationEvents(masterHtml: string): Promise<Partic
 				if (name == "") {
 					return;
 				}
-				events.push({ name, type });
+				events.push({ name, type, candidate: sched.title });
 			});
 		}
 	}
 	return events;
+}
+
+export function totalCount(counts: ParticipationCount): number {
+	let total = 0;
+	for (const kv of counts.counts) {
+		const count = kv[1];
+		total += count;
+	}
+	return total;
+}
+
+export async function getParticipation(masterHtml: string): Promise<ParticipationCount[]> {
+	const events = await getParticipationEvents(masterHtml);
+	normalizeNames(events);
+	const aggregate = aggregateParticipation(events);
+	aggregate.sort((a, b) => totalCount(b) - totalCount(a));
+	return aggregate;
+}
+
+type NameInfo = {
+	remapNames: { [key: string]: string };
+	allNames: Set<string>;
+};
+
+function inferNames(events: ParticipationEvent[]): NameInfo {
+	const remapNames: { [key: string]: string } = {};
+	const allNames = new Set<string>();
+	const firstLast = new RegExp(`^([A-Z][A-Za-z\\-]+) ([A-Z][a-z\\-]+)$`);
+	const seen: Set<string> = new Set();
+	for (const event of events) {
+		const m = firstLast.exec(event.name);
+		if (m) {
+			allNames.add(m[0]);
+			// map first name to full name
+			for (const namePart of [m[1], m[2]]) {
+				if (seen.has(namePart)) {
+					console.log(`${namePart} is ambiguous`);
+					delete remapNames[namePart];
+				} else {
+					remapNames[namePart] = event.name;
+				}
+			}
+		}
+	}
+	return { remapNames, allNames };
+}
+
+function normalizeName(nameInfo: NameInfo, name: string): string {
+	if (nameInfo.allNames.has(name)) {
+		return name;
+	}
+	for (const namePart of Object.keys(nameInfo.remapNames)) {
+		if (name.includes(namePart)) {
+			return nameInfo.remapNames[namePart];
+		}
+	}
+	return name;
+}
+
+function normalizeNames(events: ParticipationEvent[]) {
+	const nameInfo = inferNames(events);
+	for (let i = 0; i < events.length; i++) {
+		events[i].name = normalizeName(nameInfo, events[i].name);
+	}
+}
+
+export type ParticipationCount = {
+	name: string;
+	counts: Map<PartType, number>;
+	candidates: string[];
+};
+
+function aggregateParticipation(events: ParticipationEvent[]): ParticipationCount[] {
+	const counts: Map<string, { types: Map<PartType, number>; candidates: string[] }> = new Map();
+	for (const event of events) {
+		if (!counts.has(event.name)) {
+			const m = new Map();
+			for (const type of ["breakfast", "lunch", "1:1", "dinner"]) {
+				m.set(type, 0);
+			}
+			counts.set(event.name, { types: new Map(), candidates: [] });
+		}
+		const thisCount = counts.get(event.name);
+		if (!thisCount) {
+			throw new Error("assertion failed: did not initialize counts");
+		}
+		thisCount.types.set(event.type, (thisCount.types.get(event.type) || 0) + 1);
+		if (!thisCount.candidates.includes(event.candidate)) {
+			thisCount.candidates.push(event.candidate);
+		}
+	}
+	const countList: ParticipationCount[] = [];
+	for (const [name, count] of counts) {
+		countList.push({ name, counts: count.types, candidates: count.candidates });
+	}
+	return countList;
 }
--- src/routes/participation/+page.svelte
+++ src/routes/participation/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { fetchSheetHtml } from "$lib/fetch_sheet";
-	import { getParticipationEvents } from "$lib/participation";
+	import { getParticipation, totalCount, type ParticipationCount } from "$lib/participation";
 	import { Heading, Helper, Input, Label, Li, List, Spinner } from "flowbite-svelte";
 	let url: string = "";
 	let sheetHtml: string | null = null;
@@ -54,12 +54,20 @@
 		<Helper color="red"><span class="font-medium">Error:</span> {fetchError}</Helper>
 	{/if}
 	{#if sheetHtml}
-		{#await getParticipationEvents(sheetHtml)}
+		{#await getParticipation(sheetHtml)}
 			<Spinner />
-		{:then evs}
+		{:then counts}
 			<List tag="ul">
-				{#each evs as ev}
-					<Li>{ev.name} &mdash; {ev.type}</Li>
+				{#each counts as count}
+					<Li
+						>{count.name} &mdash; {totalCount(count)}
+						<span class="text-gray-500">
+							[{#each count.candidates as c, idx}
+								{c}{#if idx < count.candidates.length - 1},
+								{/if}
+							{/each}]
+						</span>
+					</Li>
 				{/each}
 			</List>
 		{/await}
