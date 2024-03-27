5708586f7a7921a428a578a34d3cb770a09bbc5c
Tej Chajed <chajed@wisc.edu>
2024-03-26 21:08:37 -0500

    Increase timeouts for fetch tests

--- src/fetch_sheet.test.ts
+++ src/fetch_sheet.test.ts
@@ -12,13 +12,13 @@ describe("fetch tsv", () => {
 			),
 		).rejects.toThrow("missing #gid");
 	});
-	it("fetches a valid sheet", async () => {
+	it("fetches a valid sheet", { timeout: 10000 }, async () => {
 		const tsv = await fetchSheetTsv(
 			"https://docs.google.com/spreadsheets/d/14c61ad_YXSYwN_uufoCh5BgiM2k5lG2Uz9KdNOjg-ZU/edit#gid=1117371261",
 		);
 		expect(tsv.length).toBeGreaterThan(10);
 	});
-	it("fetches a valid sheet by name rather than gid", async () => {
+	it("fetches a valid sheet by name rather than gid", { timeout: 10000 }, async () => {
 		const tsv = await fetchSheetTsv(
 			"https://docs.google.com/spreadsheets/d/14c61ad_YXSYwN_uufoCh5BgiM2k5lG2Uz9KdNOjg-ZU/edit",
 			"Schedule",
