b760fc8eca2e5feb855111665d7bffd361f32513
Tej Chajed <chajed@wisc.edu>
2025-05-13 21:51:42 -0500

    pnpm format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import "../app.pcss";
 	interface Props {
-		children?: import('svelte').Snippet;
+		children?: import("svelte").Snippet;
 	}
 
 	let { children }: Props = $props();
