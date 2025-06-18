37d42900cfafa37fcd97d92ed64b6165bfc874c1
Tej Chajed <chajed@wisc.edu>
2025-06-18 10:35:41 -0500

    Attempt to migrate to tailwindcss 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
+import tailwindcss from "@tailwindcss/vite";
+import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [devtoolsJson(), sveltekit(), tailwindcss()],
 	test: {
 		include: ["src/**/*.{test,spec}.{js,ts}"],
 	},
